1fa9123b6e7566c395658b8519f645c6561abaa7
Sourav Das <souravdas360@gmail.com>
2024-04-08 20:47:15 +0530

    quiz implemented

---
+++ src/components/FinishedScreen.js
@@ -0,0 +1,30 @@
+function FinishedScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if ((percentage >= 80) & (percentage < 100)) emoji = "🎉";
+  if ((percentage >= 50) & (percentage < 80)) emoji = "🔰";
+  if ((percentage >= 0) & (percentage < 50)) emoji = "🌓";
+  if (percentage === 0) emoji = "🌚";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored
+        <strong>
+          {points} out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
+        </strong>
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishedScreen;
